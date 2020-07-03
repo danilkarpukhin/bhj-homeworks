8975ad19267f726ce25a32704065da3f08bbd10d
danilkarpukhin <59309164+danilkarpukhin@users.noreply.github.com>
2020-07-03 12:27:02 +0600

    дз

---
+++ document-structure/todo/task.js
@@ -0,0 +1,41 @@
+const inputField = document.getElementById('task__input');
+const tasksList = document.getElementById('tasks__list');
+const taskButton = document.getElementById('tasks__add');
+
+function insertTask () {
+    text = inputField.value;
+
+    if (text.trim()) {
+        tasksList.insertAdjacentHTML("afterBegin",
+     `<div class="task">
+        <div class="task__title">
+            ${text}
+        </div>
+        <a href="#" class="task__remove">&times;</a>
+      </div>`);
+
+      inputField.value = null;
+    }   
+
+}
+
+inputField.addEventListener('change', e => {
+    e.preventDefault();
+    insertTask();
+  }
+);
+
+taskButton.addEventListener('click', e => {
+    e.preventDefault();
+    insertTask();
+  }
+);
+
+tasksList.addEventListener("click", (e) => {
+    let target = e.target;
+
+    if (target.classList.contains('task__remove')) {
+        target.parentElement.remove();
+    }
+    
+   });    
